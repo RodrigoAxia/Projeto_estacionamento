47ea07cb28ffb5c876994601ac07be41ffc80be2
Rodrigo Hossaki <rod_sjc@hotmail.com>
2026-08-07 19:35:52 -0300

    Alterações para logout

--- estrutura/import_js.php
+++ estrutura/import_js.php
@@ -9,3 +9,7 @@
     <script src="js/sb-admin-2.min.js"></script> 
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
+
+       <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+   <!-- Bootstrap JS -->
+   <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
